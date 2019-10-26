67b5be0ba8617a51a912091997a598258865b9d0
meharvey21 <mharve21@msudenver.edu>
2019-10-26 16:04:20 -0600

    added Nav bar and Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :makes
   resources :cars
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "cars#index"  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
