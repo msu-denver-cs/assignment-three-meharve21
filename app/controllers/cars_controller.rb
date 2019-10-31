cd1d1d8ee4df55255508058e98d91467d01a1331
meharvey21 <mharve21@msudenver.edu>
2019-10-31 11:43:08 -0600

    Edited Car_Controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -74,6 +74,6 @@ class CarsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def car_params
-      params.require(:car).permit(:name, :make_id => [])
+      params.require(:car).permit(:name, :make_id)
     end
 end
